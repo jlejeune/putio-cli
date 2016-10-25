65cac6a68e86ede68984ef20271b4e4f3c299b3f
jlejeune <mail.julien.le.jeune@gmail.com>
2016-10-25 23:06:27 +0200

    Add config command

---
+++ putio_cli/commands/config.py
@@ -0,0 +1,51 @@
+"""The config command."""
+
+import ConfigParser
+import os
+import sys
+
+
+class Config():
+    """
+    config command to manage configuration file
+
+    Usage:
+      putio-cli config <command>
+
+    Commands:
+      template                 write a template configuration file in ~/.putio-cli/config.ini
+      show                     show config file
+    """
+
+    def __init__(self, options):
+        self.options = options
+        self.cfgfilename = os.path.expanduser(self.options['--config'])
+
+        # create dir if needed
+        cfgfiledir = os.path.dirname(self.cfgfilename)
+        if not os.path.exists(cfgfiledir):
+            os.mkdir(cfgfiledir)
+
+    def run(self):
+        subcommand = self.options['<command>']
+        if subcommand == 'template':
+            if os.path.exists(self.cfgfilename) and os.path.getsize(self.cfgfilename) > 0:
+                sys.exit('Config file already exists, please fill it or delete it before launching this subcommand')
+
+            # create a template configuration file
+            cfgfile = open(self.cfgfilename, 'w')
+            config = ConfigParser.ConfigParser()
+            config.add_section('Settings')
+            config.set('Settings', 'oauth-token', 'TO REPLACE')
+            config.write(cfgfile)
+            cfgfile.close()
+
+        elif subcommand == 'show':
+            try:
+                cfgfile = open(self.cfgfilename, 'r')
+            except IOError:
+                sys.exit('Config file does not exist, please use template subcommand first')
+            print cfgfile.read()
+            cfgfile.close()
+        else:
+            sys.exit("This subcommand '%s' doesn't exist, try `putio-cli config --help`" % subcommand)
---
+++ tests/commands/test_config.py
@@ -0,0 +1,37 @@
+"""Tests for our `putio-cli config` subcommand."""
+
+
+from subprocess import PIPE, STDOUT, Popen as popen
+from unittest import TestCase
+import os
+
+
+class TestConfig(TestCase):
+    test_config_file = '/tmp/putio-cli.ini'
+
+    def test_returns_help(self):
+        output = popen(['python', 'putio-cli', 'config', '--help'], stdout=PIPE).communicate()[0]
+        self.assertTrue('Usage:' in output)
+
+    def test_create_template(self):
+        popen(['python', 'putio-cli', '--config', self.test_config_file, 'config', 'template'],
+              stdout=PIPE).communicate()[0]
+        self.assertTrue(os.path.exists(self.test_config_file))
+
+        output = popen(['python', 'putio-cli', '--config', self.test_config_file, 'config', 'template'],
+                       stdout=PIPE, stderr=STDOUT).communicate()[0]
+        self.assertEqual(output.strip(),
+                         'Config file already exists, please fill it or delete it before launching this subcommand')
+
+    def test_show_config(self):
+        output = popen(['python', 'putio-cli', '--config', self.test_config_file, 'config', 'show'],
+                       stdout=PIPE).communicate()[0]
+        self.assertEqual(output.replace('\n', '').strip(), '[Settings]oauth-token = TO REPLACE')
+
+        # clean
+        os.remove(self.test_config_file)
+
+        output = popen(['python', 'putio-cli', '--config', self.test_config_file, 'config', 'show'],
+                       stdout=PIPE, stderr=STDOUT).communicate()[0]
+        self.assertEqual(output.strip(),
+                         'Config file does not exist, please use template subcommand first')
