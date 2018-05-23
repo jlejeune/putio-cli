046ab8fc0f60b15ccdafcbb549c7de894ecd064e
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2018-05-23 22:10:10 +0200

    Remove run method (useless) in Base class

--- putio_cli/commands/base.py
+++ putio_cli/commands/base.py
@@ -12,10 +12,6 @@ class Base(object):
     def __init__(self, options):
         self.options = options
 
-    def run(self):
-        raise NotImplementedError(
-            'You must implement the run() method yourself!')
-
 
 class BaseClient(Base):
     """A base client command."""
